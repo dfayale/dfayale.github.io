068291705b66aa5ae386a64ee7bf650fd21a15d4
Christopher Nathan <christophernathan1217@gmail.com>
2021-07-20 23:53:18 -0400

    switch to using create-react-app for easy routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Projects } from './pages/Projects'
+import { Project } from './pages/Project'
+import { Outreach } from './pages/Outreach'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/about'>
+            <About/>
+        </Route>
+        <Route exact path='/projects'>
+            <Projects/>
+        </Route>
+        <Route exact path='/projects/:project'>
+            <Project/>
+        </Route>
+        <Route exact path='/outreach'>
+            <Outreach/>
+        </Route>
+        <Route path='/'>
+            <Home/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Outreach.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Outreach = () => {
+    return (
+        <p>Outreach</p>
+    )
+}
