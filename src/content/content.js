f4c29f89c7bd79d6282abaaf5cdec24001095ec1
Christopher Nathan <christophernathan1217@gmail.com>
2021-07-27 23:20:27 -0400

    implement dynamic project page structure

---
+++ src/content/content.js
@@ -0,0 +1,21 @@
+export const ProjectContent = {
+  ctsbdc: {
+    title: 'Connecticut Small Business Development Center',
+    description:
+      "Team worked in collaboration with the Connecticut Small Business Development Center (CTSBDC) to provide support for small business in the New Haven community. The team underwent a full redesign of CTSBDC's Marketplace website, which is home to promotional material for small businesses in the area. This work included the addition of a blog feature to the new site, and expansion of accessibility for businesses to benefit from the program. The team also compiled a small business guide containing content on physical as well as online presence, to be distributed to small businesses on behalf of the CTSBDC.",
+    imageLinks: [],
+  },
+  peels_and_wheels: {
+    title: 'Peels and Wheels',
+    summary:
+      'In 2019-2020, we worked with Peels and Wheels Composting to develop a Community Composting: A Shared-Bin Ecosystem to increase the accessibility of Peels and Wheel’s composting services by making the cost and scale more flexible to suit the needs of a wider range of composters.',
+    description:
+      'Through the design process, many of our initial assumptions about Peels and Wheels composters were disrupted through interviews and survey findings. We found that most composters were happy and satisfied by the service that Peels and Wheels provides, but those living in apartments faced particular challenges, such as cleaning the bucket and finding a location to leave it outside. Further, we found that single-family homes are far more likely to compost compared to other living arrangements. Through many rounds of ideating, testing, and iterating, we developed a prototype for a model of compost pick up that would make the service more cost effective, and less labor-intensive for the composters and the Peels and Wheels team. Community Composting: A Shared Bin Ecosystem specifically targets those who live in apartments, multi-family homes, condominiums, and dormitories by allowing multiple units to share one compost bin. Therefore, those who do not produce enough organic waste to justify investing in the service alone, could not afford the service by themselves, or do not have the capacity to maintain a compost bin, are able to share a bin and the responsibilities that come with maintaining it. Peels and Wheels will in turn be able to collect more compost with fewer pick-up locations.',
+    imageLinks: [
+      '/projects/pw-primary.png',
+      '/projects/pw1.png',
+      '/projects/pw2.png',
+      '/projects/pw3.png',
+    ],
+  },
+}
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { ProjectContent } from '../content/content'
 
-export const Project = () => {
-    return (
-        <p>Project</p>
-    )
+const getImages = (imageLinks) => {
+  const images = []
+  for (const imageLink of imageLinks) {
+    console.log(imageLink)
+    images.push(<img src={imageLink} />)
+  }
+  return images
+}
+
+export const Project = (props) => {
+  const { project } = useParams()
+  const { title, summary, description, imageLinks } = ProjectContent[project]
+  const images = getImages(imageLinks)
+  return (
+    <main>
+      <h1>{title}</h1>
+      <h3>{summary}</h3>
+      <p>{description}</p>
+      {images}
+    </main>
+  )
 }
