6fc7c1403717b43a523ee20afe68ab0b0614748f
Christopher Nathan <christophernathan1217@gmail.com>
2021-08-07 16:02:48 -0400

    add all project images to project page

--- src/content/content.js
+++ src/content/content.js
@@ -3,20 +3,7 @@ export const ProjectContent = {
     title: 'Connecticut Small Business Development Center',
     description:
       "Team worked in collaboration with the Connecticut Small Business Development Center (CTSBDC) to provide support for small business in the New Haven community. The team underwent a full redesign of CTSBDC's Marketplace website, which is home to promotional material for small businesses in the area. This work included the addition of a blog feature to the new site, and expansion of accessibility for businesses to benefit from the program. The team also compiled a small business guide containing content on physical as well as online presence, to be distributed to small businesses on behalf of the CTSBDC.",
-    imageLinks: [],
-  },
-  peels_and_wheels: {
-    title: 'Peels and Wheels',
-    summary:
-      'In 2019-2020, we worked with Peels and Wheels Composting to develop a Community Composting: A Shared-Bin Ecosystem to increase the accessibility of Peels and Wheel’s composting services by making the cost and scale more flexible to suit the needs of a wider range of composters.',
-    description:
-      'Through the design process, many of our initial assumptions about Peels and Wheels composters were disrupted through interviews and survey findings. We found that most composters were happy and satisfied by the service that Peels and Wheels provides, but those living in apartments faced particular challenges, such as cleaning the bucket and finding a location to leave it outside. Further, we found that single-family homes are far more likely to compost compared to other living arrangements. Through many rounds of ideating, testing, and iterating, we developed a prototype for a model of compost pick up that would make the service more cost effective, and less labor-intensive for the composters and the Peels and Wheels team. Community Composting: A Shared Bin Ecosystem specifically targets those who live in apartments, multi-family homes, condominiums, and dormitories by allowing multiple units to share one compost bin. Therefore, those who do not produce enough organic waste to justify investing in the service alone, could not afford the service by themselves, or do not have the capacity to maintain a compost bin, are able to share a bin and the responsibilities that come with maintaining it. Peels and Wheels will in turn be able to collect more compost with fewer pick-up locations.',
-    imageLinks: [
-      '/projects/pw-primary.png',
-      '/projects/pw1.png',
-      '/projects/pw2.png',
-      '/projects/pw3.png',
-    ],
+    imageLinks: ['/projects/ctsbdc-primary.jpeg'],
   },
   unilever: {
     title: 'Unilever National Project',
@@ -41,11 +28,24 @@ export const ProjectContent = {
     description:
       'For URI, we created a newly designed catalogue for each tree that URI plants, complete with a table of contents organized by whether the tree can be planted in a space with or without utility wires; pictures of the full tree in summer, fall, and winter; and descriptions of each tree with max height and spread. Furthermore, we created specific icons for full sun vs. part shade vs. full shade, fall colors, showy flowers, native species, and wildlife friendly species. For the Nucleus, we designed a website with a homepage, a donation page, a visit us and about us section, and a page for stories of people directly affected by gun violence with an opportunity for anyone to share their story or reflection on the garden.',
     imageLinks: [
-      '/projects/uri-primary.png',
+      '/projects/uri-primary.jpeg',
       '/projects/uri1.png',
       '/projects/uri2.png',
       '/projects/uri3.png',
+      '/projects/uri4.png',
     ],
+  },
+  edgewood: {
+    title: '',
+    summary: '',
+    description: '',
+    imageLinks: ['/projects/edgewood-primary.png'],
+  },
+  pathways: {
+    title: '',
+    summary: '',
+    description: '',
+    imageLinks: ['/projects/pathways-primary.jpeg'],
   },
   clinic: {
     title: 'Haven Free Clinic',
@@ -62,6 +62,37 @@ export const ProjectContent = {
       '/projects/clinic5.png',
       '/projects/clinic6.png',
     ],
+  },
+  peels_and_wheels: {
+    title: 'Peels and Wheels',
+    summary:
+      'In 2019-2020, we worked with Peels and Wheels Composting to develop a Community Composting: A Shared-Bin Ecosystem to increase the accessibility of Peels and Wheel’s composting services by making the cost and scale more flexible to suit the needs of a wider range of composters.',
+    description:
+      'Through the design process, many of our initial assumptions about Peels and Wheels composters were disrupted through interviews and survey findings. We found that most composters were happy and satisfied by the service that Peels and Wheels provides, but those living in apartments faced particular challenges, such as cleaning the bucket and finding a location to leave it outside. Further, we found that single-family homes are far more likely to compost compared to other living arrangements. Through many rounds of ideating, testing, and iterating, we developed a prototype for a model of compost pick up that would make the service more cost effective, and less labor-intensive for the composters and the Peels and Wheels team. Community Composting: A Shared Bin Ecosystem specifically targets those who live in apartments, multi-family homes, condominiums, and dormitories by allowing multiple units to share one compost bin. Therefore, those who do not produce enough organic waste to justify investing in the service alone, could not afford the service by themselves, or do not have the capacity to maintain a compost bin, are able to share a bin and the responsibilities that come with maintaining it. Peels and Wheels will in turn be able to collect more compost with fewer pick-up locations.',
+    imageLinks: [
+      '/projects/pw-primary.png',
+      '/projects/pw1.png',
+      '/projects/pw2.png',
+      '/projects/pw3.png',
+    ],
+  },
+  pg: {
+    title: '',
+    summary: '',
+    description: '',
+    imageLinks: ['/projects/iris-primary.jpeg'],
+  },
+  iris: {
+    title: '',
+    summary: '',
+    description: '',
+    imageLinks: ['/projects/pg-primary.jpeg'],
+  },
+  reads: {
+    title: '',
+    summary: '',
+    description: '',
+    imageLinks: ['/projects/reads-primary.jpeg'],
   },
   bikeshare: {
     title: 'Noa Technologies + Yale Office of Sustainability Bikeshare',
@@ -94,27 +125,47 @@ export const ProjectContent = {
     description:
       'When visiting the Yale Landscape Lab for the first time - a trip that neared 45-minutes - we initially sought to improve transportation to the YLL, or at least accessibility to its workshops and mission. But upon interviewing students and interns and volunteers, we realized that transportation difficulties made it difficult for members to meet and interact regularly, so we focused on building a stronger sense of community there. Our team completed several rounds of ideation, ultimately deciding to build at a three-sided bulletin board that would serve as a communication and interaction hub at the YLL. The panels are equipped with information about the YLL & its diverse missions, current members & running projects, and a blackboard for surveys & upcoming activities. With the panels complete and the structure upright, the team is excited to assemble the bulletin board for implementation on YLL’s campus in the fall.',
     imageLinks: [
-      '/projects/landscape-primary.jpeg',
+      '/projects/landscape-primary.png',
       '/projects/landscape1.jpeg',
       '/projects/landscape2.jpeg',
       '/projects/landscape3.jpeg',
       '/projects/landscape4.jpeg',
       '/projects/landscape5.jpeg',
+      '/projects/landscape6.jpeg',
     ],
   },
-  //   peabody: {
-  //     title: 'Yale Peabody Museum',
-  //     summary:
-  //       "The Peabody Museum is recognized as a leading science and research institution. Our team worked extensively with museum staff to bridge the gap between the museum's user experiences and stakeholder expectations in order to create a more engaging, immersive user experience. The challenge we encountered: How Might We Immerse Visitors into a Relevant, Current, and Compelling Story Inspired by the Peabody’s Research?",
-  //     description:
-  //       "Over the course of two weeks, our team members conducted interviews with users (museum-goers), otential users (students, community members), and stakeholders (museum staff).",
-  //     imageLinks: [
-  //       '/projects/landscape-primary.jpeg',
-  //       '/projects/landscape1.jpeg',
-  //       '/projects/landscape2.jpeg',
-  //       '/projects/landscape3.jpeg',
-  //       '/projects/landscape4.jpeg',
-  //       '/projects/landscape5.jpeg',
-  //     ],
-  //   },
+  peabody: {
+    title: 'Yale Peabody Museum',
+    summary:
+      "The Peabody Museum is recognized as a leading science and research institution. Our team worked extensively with museum staff to bridge the gap between the museum's user experiences and stakeholder expectations in order to create a more engaging, immersive user experience. The challenge we encountered: How Might We Immerse Visitors into a Relevant, Current, and Compelling Story Inspired by the Peabody’s Research?",
+    description:
+      'Over the course of two weeks, our team members conducted interviews with users (museum-goers), otential users (students, community members), and stakeholders (museum staff).',
+    imageLinks: ['/projects/peabody-primary.jpeg'],
+  },
+  hospital: {
+    title: 'Reducing Readmission Rates for Opiate Addicts',
+    summary:
+      'Currently, prescription pain medication abuse, as well as heroin use, are growing epidemics in the United States. How can we address this epidemic on a local scale, reducing readmission rates of addicts to Yale New Haven Hospital?',
+    description: '',
+    imageLinks: ['/projects/hospital-primary.jpeg'],
+  },
+  bethel: {
+    title: 'Reducing Childhood Trauma in Homeless Shelters',
+    summary:
+      'Members of our Beth-El Center team worked with a local homeless shelter in Milford, CT to help reduce childhood trauma. Over the course of the project, our team realized that we could not simply make the environment as appealing as possible to children because they might then create excessive positive associations with homeless shelters. Instead, we needed to focus on creating a comforting and nurturing environment for children. After prototyping an idea for a portable bathroom light and a coloring book, we delivered a coloring book to the Beth-El Center designed to acclimate children to the shelter more quickly.',
+    description: '',
+    imageLinks: ['/projects/bethel-primary.png'],
+  },
+  koffee: {
+    title: '',
+    summary: '',
+    description: '',
+    imageLinks: ['/projects/koffee-primary.jpeg'],
+  },
+  shelter: {
+    title: '',
+    summary: '',
+    description: '',
+    imageLinks: ['/projects/shelter-primary.png'],
+  },
 }
